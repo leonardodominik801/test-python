cc4450f7db92cdab79f6e9d5b6c8be4e43d63e38
Sonny Kothapally <me@sonnyksimon.com>
2019-07-06 19:37:17 -0400

    generate setup.py using paver

--- bank/pavement.py
+++ bank/pavement.py
@@ -1,5 +1,27 @@
 from paver.tasks import task, BuildFailure, needs
 from paver.easy import sh
+from paver.setuputils import setup, find_package_data
+
+
+package_data = find_package_data()
+
+entry_points = {
+    'console_scripts': [
+        'run_server = bank_app.main',
+    ]
+}
+
+
+setup(name='bank_app',
+      version='0.0.1',
+      author='Leo',
+      maintainer='Leo',
+      description='Example application to demonstrate Python testing techniques.',
+      license='License :: BSD',
+      include_package_data=True,
+      packages=['bank'],
+      package_data=package_data,
+      entry_points=entry_points)
 
 
 @task
@@ -20,7 +42,16 @@ def run_pylint():
         pass
 
 
-@needs('unit_tests', 'lettuce_tests', 'run_pylint')
+@task
+@needs('paver.misctasks.generate_setup',
+       'distutils.command.sdist')
+def sdist():
+    """Generates the setup file and packages up the
+    commercial_inventory application."""
+    pass
+
+
+@needs('unit_tests', 'lettuce_tests', 'run_pylint', 'sdist')
 @task
 def default():
     pass
