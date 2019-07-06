2e68bc4e42739641f58e2f62f297c7af7ae4a472
Sonny Kothapally <me@sonnyksimon.com>
2019-07-05 23:03:40 -0400

    mock and patch

--- app/account.py
+++ app/account.py
@@ -1,6 +1,16 @@
+import requests
+
+
 class Account(object):
     def __init__(self, data_interface):
         self.di = data_interface
 
     def get_account(self, id_num):
-        return self.di.get(id_num)
+        try:
+            result = self.di.get(id_num)
+        except ConnectionError:
+            result = "Connection error occured. Try Again."
+        return result
+
+    def get_current_balance(self, id_num):
+        return requests.get("http://some-account-uri/"+id_num)
--- test/account_test.py
+++ test/account_test.py
@@ -1,5 +1,5 @@
 import unittest
-from mock import Mock
+from mock import Mock, patch
 from app.account import Account
 
 
@@ -10,6 +10,19 @@ class TestAccount(unittest.TestCase):
         mock_data_interface.get.return_value = account_data
         account = Account(mock_data_interface)
         self.assertDictEqual(account_data, account.get_account(1))
+
+    def test_account_when_connect_exception_raised(self):
+        mock_data_interface = Mock()
+        mock_data_interface.get.side_effect = ConnectionError()
+        account = Account(mock_data_interface)
+        self.assertEqual("Connection error occured. Try Again.", 
+                         account.get_account(1))
+
+    @patch('app.account.requests')
+    def test_get_current_balance_returns_data_correctly(self, mock_requests):
+        mock_requests.get.return_value = '500'
+        account = Account(Mock())
+        self.assertEqual('500', account.get_current_balance('1'))
 
 
 if __name__ == "__main__":
