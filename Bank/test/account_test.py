811fcbe520619a6b338177d4e802b1f434e09d76
Sonny Kothapally <me@sonnyksimon.com>
2019-07-05 23:56:22 -0400

    TDD: do tests first

---
+++ Bank/test/account_test.py
@@ -0,0 +1,10 @@
+import unittest
+
+
+class TestAccount(unittest.TestCase):
+    def test_account_object_can_be_created(self):
+        account = Account()
+
+
+if __name__ == "__main__":
+    unittest.main()
