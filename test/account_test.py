13bd12e54e434a30d3e95e9937d2b00bc87bf3c0
Sonny Kothapally <me@sonnyksimon.com>
2019-07-05 23:10:09 -0400

    mock a dict's properties

--- test/account_test.py
+++ test/account_test.py
@@ -20,9 +20,13 @@ class TestAccount(unittest.TestCase):
 
     @patch('app.account.requests')
     def test_get_current_balance_returns_data_correctly(self, mock_requests):
-        mock_requests.get.return_value = '500'
+        mock_response = Mock()
+        mock_response.status_code = 200
+        mock_response.text = 'Some text data'
+        mock_requests.get.return_value = mock_response
         account = Account(Mock())
-        self.assertEqual('500', account.get_current_balance('1'))
+        self.assertEqual({'status': 200, 'data': 'Some text data'},
+                         account.get_current_balance('1'))
 
 
 if __name__ == "__main__":
