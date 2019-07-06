db3b0e117da4b06470c9fb868609307bca8113a7
Sonny Kothapally <me@sonnyksimon.com>
2019-07-06 00:28:15 -0400

    TDD: fix ['Bank' object has no attribute 'get_account_balance'] and add coveragerc (all tests have passed)

--- Bank/bank.py
+++ Bank/bank.py
@@ -4,3 +4,6 @@ class Bank(object):
 
     def add_account(self, account):
         self.accounts[account.account_number] = account.balance
+
+    def get_account_balance(self, account_number):
+        return self.accounts.get(account_number)
