645ddb237fdb79a04042b386824490ba9abe84fe
Sonny Kothapally <me@sonnyksimon.com>
2019-07-05 23:15:16 -0400

    add docstr

--- app/calculate.py
+++ app/calculate.py
@@ -3,6 +3,9 @@ from __future__ import print_function
 
 class Calculate:
     def add(self, x, y):
+        """Takes two integers and adds them together 
+        to produce the result."""
+        
         if type(x) == int and type(y) == int:
             return x + y
         else:
